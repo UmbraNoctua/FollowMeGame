457764f37f1a232a0fac34197a0ef74056aa65e6
siobhannB <64455423+siobhannB@users.noreply.github.com>
2020-05-15 08:51:50 +0800

    Interaction + dialogue
    -added scripts that allow player to interact with items.
    -now trying to figure out how to trigger dialogue when player interacts with these items.

---
+++ Follow Me/Assets/Scripts/Interactable.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+public class Interactable : MonoBehaviour
+{
+
+    public bool isInRange;
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (isInRange)
+        {
+            if (Input.GetKeyDown(interactKey))
+            {
+                interactAction.Invoke();
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+            Debug.Log("Player now in range");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+            Debug.Log("Player now not in range");
+        }
+    }
+}
