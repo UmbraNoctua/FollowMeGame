3a84da01ff38455ebc9c440e8266b27954479425
siobhannB <64455423+siobhannB@users.noreply.github.com>
2020-05-17 21:06:27 +0800

    pickup script
    changes have been made to pickup script

---
+++ Follow Me/Assets/Scripts/Inventory Scripts/PickUp.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+
+{
+    public bool PickUp;
+    RaycastHit2D hit;
+    public float distance = 2f;
+    public Transform 
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyCode(Mouse0));
+        {
+            if (!PickUp);
+            {
+                Physics2D.raycastsStartInColliders = false;
+                hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x,);
+            }
+
+            if(hit.collider!=null)
+            {
+                PickUp = true;
+            }
+
+        
+       }
+
+        if(PickUp)
+            hit.collider.gameObject.transform.position=
+
+     void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + Vector2.right * transform.localScale.x, distance);
+    }
+
+
