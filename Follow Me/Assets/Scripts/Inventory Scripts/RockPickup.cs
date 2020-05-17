1e98b180e744c50490eb4ac04d9208822b158d9f
siobhannB <64455423+siobhannB@users.noreply.github.com>
2020-05-17 18:39:31 +0800

    Inventory + Monster
    -working on the monster section

---
+++ Follow Me/Assets/Scripts/Inventory Scripts/RockPickup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockPickup : MonoBehaviour
+{
+    private Inventory inventory;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventory.Slots.Length; i++)
+            {
+                if (inventory.isFull[i]) == false) 
+            {
+                inventory.isFull[i] = true;
+            }
+
+        }
+    }
+}
