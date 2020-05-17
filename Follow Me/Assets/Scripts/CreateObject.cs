20257ae4eaa74e5139d7ee6779d0a0a75de5c1db
siobhannB <64455423+siobhannB@users.noreply.github.com>
2020-05-17 16:13:40 +0800

    wisp
    -first trigger event with wisp has been added

---
+++ Follow Me/Assets/Scripts/CreateObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObject : MonoBehaviour
+{
+    public Transform Spawnpoint;
+    public GameObject Prefab;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Instantiate(Prefab, Spawnpoint.position, Spawnpoint.rotation);
+    }
+}
