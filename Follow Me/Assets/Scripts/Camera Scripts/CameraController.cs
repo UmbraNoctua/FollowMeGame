f93dba7360eaf84eee64c3dc3a462fed56afe107
grapesodart <jacobeverest@yahoo.com.au>
2020-08-24 16:06:12 +0800

    Fadeout Transitions between Menu and Level 1, LateUpdate on Main Camera

--- Follow Me/Assets/Scripts/Camera Scripts/CameraController.cs
+++ Follow Me/Assets/Scripts/Camera Scripts/CameraController.cs
@@ -17,7 +17,7 @@ public class CameraController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         if (transform.position != target.position)
         {
--- Follow Me/Assets/Scripts/Menu Scripts/MainMenu.cs
+++ Follow Me/Assets/Scripts/Menu Scripts/MainMenu.cs
@@ -5,11 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    
+    public Animator transition;
+    public float transitionTime = 1f;
+
+
     public void NewJourney ()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
         //calls whaterver scene is in the build index exactly after this one.
+        //calls transition function LoadLevel below
     }
+
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //Play Animation
+        transition.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(1);
+
+        //Load Scene
+        SceneManager.LoadScene(levelIndex);
+
+    }
+
+
     public void QuitGame()
     {
         Debug.Log("QUIT!"); //checks that the quit button is working by printing the word quit
