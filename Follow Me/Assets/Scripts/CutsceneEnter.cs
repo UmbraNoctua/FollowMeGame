7aed7992c47f9df446fb5d25be5e910d19f044ef
siobhannB <64455423+siobhannB@users.noreply.github.com>
2020-10-08 15:23:53 +0800

    the hub
    -dialogue added for interactable objects- firepit, bridge, brambles
    -map added
    -dialogue not working with bridge and brambles though
    
    note: camera needs to be fixed so we can see the character

---
+++ Follow Me/Assets/Scripts/CutsceneEnter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneEnter : MonoBehaviour
+{
+    public GameObject thePlayer;
+    public GameObject cutsceneCam;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        cutsceneCam.SetActive(true);
+        thePlayer.SetActive(false);
+        StartCoroutine(FinishCut());
+    }
+
+    IEnumerator FinishCut()
+    {
+        yield return new WaitForSeconds(10);
+        thePlayer.SetActive(true);
+        cutsceneCam.SetActive(false);
+    }
+}
