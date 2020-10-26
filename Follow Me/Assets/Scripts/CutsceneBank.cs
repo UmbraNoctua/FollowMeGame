a9279d2dff02a4b09a62b1229767ecdf3a091277
grapesodart <jacobeverest@yahoo.com.au>
2020-10-26 15:35:15 +0800

    Dialogue Cutscene Setup

---
+++ Follow Me/Assets/Scripts/CutsceneBank.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "CutsceneSprites")]
+
+public class CutsceneBank : ScriptableObject
+{
+
+    public const string CUTSCENE1 = "Cutscene1";
+    public const string CUTSCENE2 = "Cutscene2";
+    public const string CUTSCENE3 = "Cutscene3";
+
+    public string cutsceneName;
+    public Sprite cutscene1, cutscene2, cutscene3;
+    
+    public Sprite GetCutscene(string cutscene)
+    {
+        switch (cutscene)
+        {
+            default:
+            case CUTSCENE1: return cutscene1;
+            case CUTSCENE2: return cutscene2;
+            case CUTSCENE3: return cutscene3;
+        }
+    }
+}
