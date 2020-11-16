d33affd6d9ab8ca12baf4d11168fd1c142f62fd2
siobhannB <64455423+siobhannB@users.noreply.github.com>
2020-11-16 08:44:47 +0800

    updated audio sources added

---
+++ Follow Me/Assets/Scripts/EntryDialogue.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class EntryDialogue : MonoBehaviour
+{
+    public YarnProgram scriptToLoad;
+
+    public DialogueRunner dialogueRunner;
+
+    private GameObject target;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        target = GameObject.FindWithTag("Player");
+    }
+    void Start()
+    {
+        dialogueRunner.Add(scriptToLoad);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Follow Me/Assets/Scripts/walkdirt_audio.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class walkdirt_audio : MonoBehaviour
+{
+    public AudioClip firstAudioClip;
+    public AudioClip secondAudioClip;
+    public AudioClip thirdAudioClip;
+    public AudioClip fourthAudioClip;
+    public AudioClip fifthAudioClip;
+    public AudioClip sixthAudioClip;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter()
+    {
+        audioSource.PlayOneShot(firstAudioClip, 0.3f);
+        audioSource.PlayOneShot(secondAudioClip, 0.3f);
+        audioSource.PlayOneShot(thirdAudioClip, 0.3f);
+        audioSource.PlayOneShot(fourthAudioClip, 0.3f);
+        audioSource.PlayOneShot(fifthAudioClip, 0.3f);
+        audioSource.PlayOneShot(sixthAudioClip, 0.3f);
+        
+    }
+}
