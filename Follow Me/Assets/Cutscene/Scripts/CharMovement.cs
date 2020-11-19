221ce04bea9863961f2ee337f31e7fc29173614f
siobhannB <64455423+siobhannB@users.noreply.github.com>
2020-11-20 07:45:14 +0800

    owl cutscene added

---
+++ Follow Me/Assets/Cutscene/Scripts/CharMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMovement : MonoBehaviour
+{
+    public float moveSpeed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        transform.position += (v * Vector3.up + h * Vector3.right) * moveSpeed * Time.deltaTime;
+    }
+
+}
---
+++ Follow Me/Assets/Cutscene/Scripts/Cutscene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cutscene : MonoBehaviour
+{
+    //This needs to be set to just more than the length of the animation.
+    public float cutSceneTime = 3.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, cutSceneTime); //destroy after animation is done.
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Destroy(gameObject); //destroy immediately
+        }
+    }
+}
