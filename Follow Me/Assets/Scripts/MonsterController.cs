bb2d980a2156adff939182d5b2aeceb98c691a3f
siobhannB <64455423+siobhannB@users.noreply.github.com>
2020-11-19 08:12:00 +0800

    slight changes again

--- Follow Me/Assets/Scripts/MonsterController.cs
+++ Follow Me/Assets/Scripts/MonsterController.cs
@@ -7,18 +7,15 @@ public class MonsterController : MonoBehaviour
 {
     enum MonsterState
     {
-        IDLE,
-        CHARGING,
+        
         RESTING,
-        STUNNED,
-        SCARED
+     
     }
 
     public Transform player;
     public float moveSpeed = 5f;
     public float distUntilRest = 3.0f;
-    public float restTime = 2.0f;
-    public float stunnedTime = 2.0f;
+   
 
     private Rigidbody2D rb;
     private MonsterState state;
@@ -26,7 +23,7 @@ public class MonsterController : MonoBehaviour
     private Animator anim;
     private float distanceTravelled;
     private float restTimer = 0.0f;
-    private float stunnedTimer = 0.0f;
+    
 
     void Start()
     {
@@ -35,51 +32,9 @@ public class MonsterController : MonoBehaviour
         
     }
 
-    void Update()
-    {
-        if (state == MonsterState.CHARGING)
-        {
-            if (distanceTravelled >= distUntilRest )
-            {
-                Rest();
-            }
-        }
-        /*
-        if (state == MonsterState.RESTING)
-        {
-            restTimer += Time.deltaTime;
-            if (restTimer > restTime)
-            {
-                Idle();
-            }
-        }
-        */
-        if (state == MonsterState.STUNNED)
-        {
-            stunnedTimer += Time.deltaTime;
-            if (stunnedTimer > stunnedTime)
-            {
-                Idle();
-            }
-        }
-    }
 
-    public void Idle()
-    {
-        state = MonsterState.IDLE;
-        restTimer = 0.0f;
-        distanceTravelled = 0.0f;
-        stunnedTimer = 0.0f;
-        anim.Play("Monster_Idle");
-    }
-
-    public void Charge(Vector2 position)
-    {
-        distanceTravelled = 0.0f;
-        destination = position;
-        state = MonsterState.CHARGING;
-    }
-
+  
+   
     public void Rest()
     {
         state = MonsterState.RESTING;
@@ -88,32 +43,10 @@ public class MonsterController : MonoBehaviour
         //GetComponent<Collider2D>().enabled = false;
         rb.isKinematic = true;
         rb.velocity = Vector2.zero;
+        anim.Play("Monster_Sleeping");
     }
 
-    public void Stunned()
-    {
-        state = MonsterState.STUNNED;
-        stunnedTimer = 0.0f;
-        anim.Play("Monster_Dazed");
-    }
-
-    private void FixedUpdate()
-    {
-        if (state == MonsterState.CHARGING)
-        {
-            if (distanceTravelled < distUntilRest)
-            {
-                Vector2 direction = destination - (Vector2)transform.position;
-                direction.Normalize();
-                moveCharacter(direction);
-            }
-            else
-            {
-                Stunned();
-            }
-        }
-    }
-
+   
     void moveCharacter(Vector2 direction)
     {
         Vector2 movement = direction * moveSpeed * Time.deltaTime; 
@@ -128,27 +61,14 @@ public class MonsterController : MonoBehaviour
         {
             //leave the player, their behaviour is handled by the events
         }
+
+        //monster collides with rock they run off.
         else if (col.collider.tag == "PickUp") //rocks are tagged with pickup
         {
             GameObject rock = col.collider.gameObject;
             Destroy(rock);
-            Charge(player.position);
+           
         }
-        else if (col.collider.tag == "Log")
-        {
-            if (state == MonsterState.CHARGING)
-            {
-                GameObject log = col.collider.gameObject;
-                Destroy(log);
-                Rest();
-                
-                //now move!
-                GetComponent<ObjectMover>().MoveObject("monster_fight_end");
-            }
-        }
-        else
-        {
-            Stunned();
-        }
+       
     }
 }
